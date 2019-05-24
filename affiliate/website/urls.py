b83257efa2a82e1a92d2bc41a515888a1aa34bc4
Yossi <yossi.kayhko@gmail.com>
2019-05-25 00:08:14 +0300

    works1

--- affiliate/website/urls.py
+++ affiliate/website/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
 	path('mega_millions/', views.mega_millions, name="mega_millions"),
 	path('cash4life/', views.cash4life, name="cash4life"),
 
-	path('winner/<int:article_id>/', views.winners, name="winners"),
+	path('winner/<int:article_id>/', views.winner_article, name="winners"),
 ]
