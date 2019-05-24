c6e2625867e4fb32b21212d1ce9551e0c3c2a912
Yossi <yossi.kayhko@gmail.com>
2019-05-24 23:15:23 +0300

    Static_root

--- affiliate/affiliate/settings.py
+++ affiliate/affiliate/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uh9wgj6r8+hi(2e5*yam+00+ule$omu!)s#wd-$cd+ahrj_20d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yoseflotter.pythonanywhere.com']
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = '/home/yosefLotter/affiliate/affiliate/static'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'assets'),
 )
@@ -128,4 +128,4 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL ='/media/'
 
-#test comment 
+# Test B
