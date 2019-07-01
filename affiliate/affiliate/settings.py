789396e4d23865568bb50cc132a979c9e5bf7b25
yosefLotter <yosef.lotter@outlook.com>
2019-07-01 12:01:38 +0300

    ckeditor

--- affiliate/affiliate/settings.py
+++ affiliate/affiliate/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['yoseflotter.pythonanywhere.com']
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'assets'),
