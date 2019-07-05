a0d37903b7e663c6985e552dffdf27f3ca72b253
yosefLotter <yosef.lotter@outlook.com>
2019-07-05 20:15:44 +0300

    404page

--- affiliate/affiliate/settings.py
+++ affiliate/affiliate/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uh9wgj6r8+hi(2e5*yam+00+ule$omu!)s#wd-$cd+ahrj_20d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['yoseflotter.pythonanywhere.com']
 
