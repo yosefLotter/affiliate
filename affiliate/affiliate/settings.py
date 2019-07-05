d88f9107667301131c08264aa5b96962edf42cb3
yosefLotter <yosef.lotter@outlook.com>
2019-07-05 20:11:56 +0300

    favicon

--- affiliate/affiliate/settings.py
+++ affiliate/affiliate/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+#   <link rel="shortcut icon" type="image/jpg" href="static/favicon.jpg">
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
